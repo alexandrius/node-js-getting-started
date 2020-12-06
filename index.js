b4856eea22ca5e9fa1f6d0b6cd2f239610bd3ae8
Alexander Pataridze <s.pataridze@gmail.com>
2020-12-06 16:19:46 +0400

    commit

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const url = require('url');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   if(req.url.includes('writeTemp')){
-    
+    if(req.method === 'GET'){
+      const current_url = new URL('http://host.com'+req.url);
+      const search_params = current_url.searchParams;
+      res.write(`[GET] = Temperature data written ${search_params}`)
+      res.end();
+    }else{
+      var body = ''
+      req.on('data', function(data) {
+        body += data
+        
+        console.log('Partial body: ' + body)
+      })
 
-    const current_url = new URL('http://host.com'+req.url);
-    const search_params = current_url.searchParams;
-
-
-    res.write(`Temperature data written ${search_params}`)
-    // fs.writeFile('data.json', '[{}]', function (err) {
-    //   if (err) return console.log(err);
-    // });
+      req.on('end', function() {
+        res.write(`[POST] = Temperature data written ${JSON.stringify(body)}`)
+        res.end();
+      })
+    }
   }else{
     res.write('endpoint not found')
+    res.end();
   }
 
-  res.end();
 }).listen(process.env.PORT || 5000);
