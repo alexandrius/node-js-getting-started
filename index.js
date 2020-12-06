d473e660e6003bf8063f02f1dc691f41f8a7e111
Alexander Pataridze <s.pataridze@gmail.com>
2020-12-06 16:06:11 +0400

    remove unused files

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const http = require('http');
+const url = require('url');
+// const fs = require('fs');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  if(req.url.includes('writeTemp')){
+    
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    const current_url = new URL('http://host.com'+req.url);
+    const search_params = current_url.searchParams;
+
+
+    res.write(`Temperature data written ${search_params}`)
+    // fs.writeFile('data.json', '[{}]', function (err) {
+    //   if (err) return console.log(err);
+    // });
+  }else{
+    res.write('endpoint not found')
+  }
+
+  res.end();
+}).listen(8080);
