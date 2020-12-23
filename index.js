9df156b9ca1b4eff712301918f2d20e344b2db52
Alexander Pataridze <s.pataridze@gmail.com>
2020-12-23 22:10:49 +0400

    add chat server

--- index.js
+++ index.js
@@ -1,81 +1,41 @@
-const http = require("http");
-const url = require("url");
+var app = require("express")();
+var http = require("http").Server(app);
 const fs = require("fs");
 const dayjs = require("dayjs");
+var io = require("socket.io")(http, {
+   cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+   },
+   origins: "*:*",
+});
+var port = process.env.PORT || 3000;
 
-const logfile = "./log2.json";
+const logfile = "./log.json";
 
-const row = (html) => `<tr>\n${html}</tr>\n`,
-   heading = (object) =>
-      row(
-         Object.keys(object).reduce(
-            (html, heading) => html + `<th>${heading}</th>`,
-            ""
-         )
-      ),
-   datarow = (object) =>
-      row(
-         Object.values(object).reduce(
-            (html, value) => html + `<td>${value}</td>`,
-            ""
-         )
-      );
+app.get("/", function (req, res) {
+   res.sendFile(__dirname + "/index.html");
+});
 
-function htmlTable(dataList) {
-   return `<table>
-            ${heading(dataList[0])}
-            ${dataList.reduce((html, object) => html + datarow(object), "")}
-          </table>`;
-}
-
-function writeChanges(res, method, data) {
-   fs.readFile(logfile, function (err, buf) {
-      if (data) {
-         const output = `[${method}] | Temperature data written = ${data}`;
-         res.write(`Current - ${output}`);
-         res.write("</br>");
-         res.write("</br>");
-         res.write("</br>");
+io.on("connection", function (socket) {
+   socket.on("chat message", function (msg) {
+      fs.readFile(logfile, function (err, buf) {
          const logArray = [
-            { date: dayjs().format(), value: output },
+            { date: dayjs().format(), value: msg },
             ...JSON.parse(buf.toString()),
          ];
-         res.write(htmlTable(logArray));
          fs.writeFileSync(logfile, JSON.stringify(logArray));
-      } else {
-         const logArray = JSON.parse(buf.toString());
-         if (logArray.length > 0) res.write(htmlTable(logArray));
-      }
-
-      res.end();
+      });
+      io.emit("chat message", msg);
    });
-}
+   socket.on("get logs", function (msg) {
+      fs.readFile(logfile, function (err, buf) {
+         console.log(buf.toString());
+      });
+      io.emit("chat message", msg);
+   });
+});
 
-http
-   .createServer(function (req, res) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      if (req.url.includes("writeTemp")) {
-         if (req.method === "GET") {
-            const current_url = new URL("http://host.com" + req.url);
-            const search_params = current_url.searchParams;
-            writeChanges(res, req.method, search_params);
-         } else {
-            let body = "";
-            req.on("data", function (data) {
-               body += data;
-               console.log("Partial body: " + body);
-            });
-            req.on("end", function () {
-               writeChanges(res, req.method, JSON.stringify(body));
-            });
-         }
-      } else if (req.url === "/readTemp") {
-         writeChanges(res, req.method);
-      } else {
-         res.write("Port = " + process.env.PORT);
-         res.write("</br>");
-         res.write("endpoint not found");
-         res.end();
-      }
-   })
-   .listen(process.env.PORT || 5000);
+http.listen(port, function () {
+   console.log("listening on *:" + port);
+});
---
+++ index_old.js
@@ -0,0 +1,81 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const dayjs = require("dayjs");
+
+const logfile = "./log.json";
+
+const row = (html) => `<tr>\n${html}</tr>\n`,
+   heading = (object) =>
+      row(
+         Object.keys(object).reduce(
+            (html, heading) => html + `<th>${heading}</th>`,
+            ""
+         )
+      ),
+   datarow = (object) =>
+      row(
+         Object.values(object).reduce(
+            (html, value) => html + `<td>${value}</td>`,
+            ""
+         )
+      );
+
+function htmlTable(dataList) {
+   return `<table>
+            ${heading(dataList[0])}
+            ${dataList.reduce((html, object) => html + datarow(object), "")}
+          </table>`;
+}
+
+function writeChanges(res, method, data) {
+   fs.readFile(logfile, function (err, buf) {
+      if (data) {
+         const output = `[${method}] | Temperature data written = ${data}`;
+         res.write(`Current - ${output}`);
+         res.write("</br>");
+         res.write("</br>");
+         res.write("</br>");
+         const logArray = [
+            { date: dayjs().format(), value: output },
+            ...JSON.parse(buf.toString()),
+         ];
+         res.write(htmlTable(logArray));
+         fs.writeFileSync(logfile, JSON.stringify(logArray));
+      } else {
+         const logArray = JSON.parse(buf.toString());
+         if (logArray.length > 0) res.write(htmlTable(logArray));
+      }
+
+      res.end();
+   });
+}
+
+http
+   .createServer(function (req, res) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      if (req.url.includes("writeTemp")) {
+         if (req.method === "GET") {
+            const current_url = new URL("http://host.com" + req.url);
+            const search_params = current_url.searchParams;
+            writeChanges(res, req.method, search_params);
+         } else {
+            let body = "";
+            req.on("data", function (data) {
+               body += data;
+               console.log("Partial body: " + body);
+            });
+            req.on("end", function () {
+               writeChanges(res, req.method, JSON.stringify(body));
+            });
+         }
+      } else if (req.url === "/readTemp") {
+         writeChanges(res, req.method);
+      } else {
+         res.write("Port = " + process.env.PORT);
+         res.write("</br>");
+         res.write("endpoint not found");
+         res.end();
+      }
+   })
+   .listen(process.env.PORT || 5000);
